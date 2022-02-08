80d14ca1b189c21b5b10a8cf9f59e2d3b7aeb06a
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 09:29:33 -0800

    Add files via upload

---
+++ ArrayLists/ArrayLists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+
+namespace ArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // declaring an ArrayList - Requires using System.Collections above
+            ArrayList myArrayList = new ArrayList(); // undefined amount of objects
+            ArrayList myArrayList2 = new ArrayList(100); // defined with 100 objects
+
+            myArrayList.Add(25);
+            myArrayList.Add("hello");
+            myArrayList.Add(13.37);
+            myArrayList.Add(13);
+            myArrayList.Add(128);
+            myArrayList.Add(25.3);
+            myArrayList.Add(13);
+
+            Console.WriteLine(myArrayList.Count);
+            myArrayList.Remove(13); //delete element with specific value the array list - the first element it finds
+            myArrayList.RemoveAt(0); // delete element at a specific position in the ArrayList
+            Console.WriteLine(myArrayList.Count);
+
+            double sum = 0;
+            foreach(object obj in myArrayList)  // object is the highest level class there is, everything inherits from it
+            {
+                if(obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }
+                else if(obj is double)
+                {
+                    sum += (double)obj;
+                }
+                else if(obj is string)
+                {
+                    Console.WriteLine($"The string is {obj}");
+                }
+
+                Console.WriteLine($"The running sum is {sum}");
+            }
+
+            Console.WriteLine($"The final sum total = {sum}");
+        }
+    }
+}
---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  This code functions perfectly in Main
+
+            List<int> numbers = new List<int>();
+            List<int> even_numbers = new List<int>();
+            for (int start = 100; start <= 170; start++)
+            {
+                numbers.Add(start);
+            }
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    even_numbers.Add(number);
+                }
+            }
+
+            Console.WriteLine($"The Even Numbers list contains {even_numbers.Count} items");
+
+            Console.WriteLine("The even numbers from 100 to 170 are: ");
+
+
+            for (int i = 0; i < even_numbers.Count; i++)
+            {
+                Console.Write(even_numbers[i] + " ");
+            }
+
+            //Console.Writeline($"The even numbers from 100 -170 are: {foreach(i in even_numbers){Console.Writeline(i))}}");
+
+        }
+
+        //this is the solution to the exercise that was in the course itself
+        public static List<int> Solution()
+        {
+            // TODO: write your solution here
+            List<int> numbers = new List<int>();
+            List<int> even_numbers = new List<int>();
+            for (int start = 100; start <= 170; start++)
+            {
+                numbers.Add(start);
+            }
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    even_numbers.Add(number);
+                }
+            }
+
+            return even_numbers;
+        }
+    }
+}
